1f1535995dd956cf4028116a375c062c22dfe939
Rosa-Fox <rosafox89@gmail.com>
2015-11-14 14:55:39 +0000

    Tops up balance on initialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def after_initialize
+    if new_record?
+      balance ||= 50
+    end
+  end
 end
